a9ff2ac988277b99caabee1dde392167d0db64ed
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2018-11-19 00:08:04 +0100

    updated client

--- src/client/MyClient.java
+++ src/client/MyClient.java
@@ -11,27 +11,34 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 
-public class MyClient {
+public class MyClient 
+{
+	private static final String MY_URI = "http://localhost:8080/dss-webservices";
+	
+	public static void main(String[] args) 
+	{
+		WebTarget target = configureClient();
 
-	public static void main(String[] args) {
-		ClientConfig config = new ClientConfig();
-
-		Client client = ClientBuilder.newClient(config);
-
-		WebTarget target = client.target(getBaseURI());
-
-		String response = target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(Response.class).toString();
-		String plainAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		String xmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
-		String htmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
+		String response = target.path("rest").path("book").request().accept(MediaType.TEXT_PLAIN).get(Response.class).toString();
+		String plainAnswer = target.path("rest").path("book").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		String xmlAnswer = target.path("rest").path("book").request().accept(MediaType.TEXT_XML).get(String.class);
+		String htmlAnswer = target.path("rest").path("book").request().accept(MediaType.TEXT_HTML).get(String.class);
 
 		System.out.println("Text plain: " + response);
 		System.out.println("Text plain: " + plainAnswer);
 		System.out.println("Text XML: "  + xmlAnswer);
 		System.out.println("Text HTML: " + htmlAnswer);
 	}
+	
+	private static WebTarget configureClient()
+	{
+		ClientConfig config = new ClientConfig();
 
-	private static URI getBaseURI() {
-		return UriBuilder.fromUri("http://localhost:8080/dss-webservices").build();
+		Client client = ClientBuilder.newClient(config);
+
+		URI uri = UriBuilder.fromUri(MY_URI).build();
+
+		return client.target(uri);
 	}
+
 }
